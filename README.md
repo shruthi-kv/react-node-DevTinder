721ca98ad3dde955bef5287cea9e2c76ddf1ae81
Shruthi KV <shruthikv@Mac.lan>
2025-07-20 09:55:11 +0530

    login API

--- README.md
+++ README.md
@@ -10,3 +10,14 @@ create APIs
     1.Sign-up - models, routes
     npm i bcrypt
     npm i validator
+
+
+{
+  "firstName":"Shruthi",
+  "lastName":"KV",
+  "email":"Shruthi@gmai.com",
+  "password":"Shruthi@123"
+}
+
+     2.Login - models, routes
+     npm i jsonwebtoken
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,6 +3,7 @@ const authRouter = express.Router();
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
 const {validateSignUpData} = require('../utils/validators')
+const jwt = require('jsonwebtoken')
 
 authRouter.post("/signup", async(req,res) =>{
     try{
@@ -26,5 +27,29 @@ authRouter.post("/signup", async(req,res) =>{
     }
 })
 
+
+authRouter.post("/login", async(req,res)=>{
+    const {email, password} = req.body;
+    try{
+
+        let user = await User.findOne({email:email});
+        if(!user){
+            throw new Error("Email doesn't exist in the DB")
+        }
+
+        let passwordMatch = await bcrypt.compare(password, user.password);
+        if(passwordMatch){
+            const token = await jwt.sign({_id:user._id }, "DEV@TINDER@2025", {expiresIn:'1d'});
+            res.cookie("token", token);
+
+            res.send("User logged in Successfully")
+        }else{
+            res.status(401).send("Password does not match")
+        }
+
+    }catch(error){
+        res.status(400).send("ERROR : " + error.message);
+    }
+})
 
 module.exports = authRouter;
